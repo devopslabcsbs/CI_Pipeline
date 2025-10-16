6df5fe085d53128d01c1ac19f355b894bc1dc3bb
devopslabcsbs <dscecsbslab@gmail.com>
2025-10-16 01:11:19 +0000

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, DevOps CI/CD using GitHub Actions!");
+        System.out.println("DevOps CI/CD using GitHub Actions!");
     }
 }
